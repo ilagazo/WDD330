8ea96f84126f8e91b83954e949677718dfb55cff
ilagazo <lag14002@byui.edu>
2021-06-08 19:40:00 -0600

    Adding link to w08 notes

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ var menuArray = [
     {label: "Week 03 Notes", url:"../WDD330/Week03/index03.html"},
     {label: "Week 04 Notes", url:"../WDD330/Week04/index04.html"},
     {label: "Week 05 Notes", url:"../WDD330/Week05/index05.html"},
-    {label: "Week 07 Notes", url:"../WDD330/Week07/index07.html"}
+    {label: "Week 07 Notes", url:"../WDD330/Week07/index07.html"},
+    {label: "Week 08 Notes", url:"../WDD330/Week08/index08.html"}
 ]
 
 function dynamicTableLoader() {
