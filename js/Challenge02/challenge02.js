d5ce5e73c21566d32b0c3a71158c52ac224106f9
ilagazo <lag14002@byui.edu>
2021-06-26 22:42:58 -0600

    Adding Part 1 functionality for challenge 02

---
+++ js/Challenge02/challenge02.js
@@ -0,0 +1,93 @@
+document.querySelector('#requestLocation').addEventListener('click', getLocation);
+document.querySelector('#requestQuery').addEventListener('click', getQuery);
+
+function getLocation() {
+    // Initialize variables for Google URL & user's current location
+    const currentLocation = document.querySelector('#currentLocation');
+    const googleURL = document.querySelector('#google-map-link-1');
+
+    googleURL.href = '';
+    googleURL.textContent = '';
+
+    // If the User gives location then do this
+    function success(position) {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+
+        currentLocation.textContent = '';
+        googleURL.href = `https://www.google.com/maps/@${lat},${long}`;
+        googleURL.textContent = `Latitude: ${lat} °, Longitude: ${long} °`;
+    }
+
+    // Handles the errors that may occur
+    function showError(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                currentLocation.textContent = "User has denied access. Unable to retrieve your location."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                currentLocation.textContent = "Position information is unavailable. Please try again"
+                break;
+            case error.TIMEOUT:
+                currentLocation.textContent = "Request to retrieve user's location has timed out. Please get a better CPU"
+                break;
+            case error.UNKNOWN_ERROR:
+                currentLocation.textContent = "Unknown error. Please try again"
+                break;
+        }
+    }
+
+    // Controller for success or error functions
+    if(!navigator.geolocation) {
+        currentLocation.textContent = "Geolocation API is not supported by the current browser.";
+    } else {
+        currentLocation.textContent = "Locating Search Query...";
+        navigator.geolocation.getCurrentPosition(success, showError);
+    }
+}
+
+function getQuery() {
+    // Initialize variables for Google URL & user's current location
+    const currentLocation = document.querySelector('#queryLocation');
+    const googleURL = document.querySelector('#google-map-link-2');
+    const query = document.querySelector('#searchQuery').value;
+
+    googleURL.href = '';
+    googleURL.textContent = '';
+
+    // If the User gives location then do this
+    function success(position) {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+
+        currentLocation.textContent = '';
+        googleURL.href = `https://www.google.com/maps/search/${query}/@${lat},${long}`;
+        googleURL.textContent = `Latitude: ${lat} °, Longitude: ${long} °`;
+    }
+
+    // Handles the errors that may occur
+    function showError(error) {
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+                currentLocation.textContent = "User has denied access. Unable to retrieve your location."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                currentLocation.textContent = "Position information is unavailable. Please try again"
+                break;
+            case error.TIMEOUT:
+                currentLocation.textContent = "Request to retrieve user's location has timed out. Please get a better CPU"
+                break;
+            case error.UNKNOWN_ERROR:
+                currentLocation.textContent = "Unknown error. Please try again"
+                break;
+        }
+    }
+
+    // Controller for success or error functions
+    if(!navigator.geolocation) {
+        currentLocation.textContent = "Geolocation API is not supported by the current browser.";
+    } else {
+        currentLocation.textContent = "Locating Search Query...";
+        navigator.geolocation.getCurrentPosition(success, showError);
+    }
+}
